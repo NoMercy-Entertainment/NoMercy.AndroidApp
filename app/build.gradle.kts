cf70883acfe160c8b9ab7c4c4ca6c8a0dcca5f56
Stoney_Eagle <45034970+StoneyEagle@users.noreply.github.com>
2025-10-25 14:18:06 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "tv.nomercy.app"
         minSdk = 28
-        targetSdk = 30
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
